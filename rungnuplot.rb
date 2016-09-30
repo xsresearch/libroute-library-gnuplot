f2e723d8f146c4acdedf4e0497b808a1603f0212
David Lisk <david.lisk@xsresearch.co.uk>
2016-09-30 18:34:00 +0000

    Write additional parameters to files
    Signed-off-by: David Lisk <david.lisk@xsresearch.co.uk>

--- rungnuplot.rb
+++ rungnuplot.rb
@@ -3,18 +3,36 @@ require 'bson'
 
 def rungnuplot(params)
   cmd = 'gnuplot'
-  inp = "#{params['stdin'].data}"
+  inp = "#{params['input'].data}"
   out = ''
   err = ''
 
-  Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-    stdin.write inp
-    stdin.close
-    out = stdout.read
-    err = stderr.read
+  Dir.chdir(Dir.home)
+
+  params.keys.each do |k|
+    if k.eql?('input')
+      next
+    end
+    File.write(k, params[k].data)
+  end
+
+  begin
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      stdin.write inp
+      stdin.close
+      out = stdout.read
+      err = stderr.read
+    end
+  ensure
+    params.keys.each do |k|
+      if k.eql?('input')
+        next
+      end
+      File.exists?(k) ? File.delete(k) : ''
+    end
   end
 
   result = Hash.new
-  result['stdout'] = BSON::Binary.new(out)
+  result['output'] = BSON::Binary.new(out)
   return result
 end
